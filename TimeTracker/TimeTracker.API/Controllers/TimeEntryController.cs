125b59e8a1291206d6cc9836db864a1d6406eacb
Taren Fairbrother <tarenfairbrother@gmail.com>
2025-02-10 22:02:58 -0700

    Initial commit and solution creation

---
+++ TimeTracker/TimeTracker.API/Controllers/TimeEntryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Shared.Entities;
+
+namespace TimeTracker.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TimeEntryController : ControllerBase
+{
+    private static List<TimeEntry> timeEntries = new List<TimeEntry>
+    {
+        new TimeEntry { Id = 1, Project = "Time Tracker App", Start = DateTime.Now.AddHours(1) }
+    };
+
+    [HttpGet]
+    [Route("all")]
+    public ActionResult<List<TimeEntry>> GetAllTimeEntries()
+    {
+        return Ok(timeEntries);
+    }
+}
