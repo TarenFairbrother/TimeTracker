1849c4c0c08679c5cc4343dc9974f093ba777377
Taren Fairbrother <tarenfairbrother@gmail.com>
2025-02-16 12:19:54 -0700

    Moved directories

--- TimeTracker/TimeTracker.Shared/Models/TimeEntry/TimeEntryCreateRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TimeTracker.Shared.Models.TimeEntry;
-
-public class TimeEntryCreateRequest
-{
-    public required string Project { get; set; }
-    public DateTime Start { get; set; } = DateTime.Now;
-    public DateTime? End { get; set; }
-}
